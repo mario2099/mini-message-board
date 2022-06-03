80bbc355568533657461a00cc30a207d6d1a457d
Mario De Leon <mariodeleon3701@gmail.com>
2022-06-03 14:50:33 -0500

    4o commit: post messages

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Mini Messageboard", messages });
 });
 
+/* GET form page. */
+router.get('/new', function(req, res, next) {
+  res.render('form', {});
+});
+
+/*POST form page input values and print them into home page then redirect to home page */
+router.post('/new', function(req, res, next){
+  messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
+  res.redirect('/');
+});
+
 module.exports = router;
